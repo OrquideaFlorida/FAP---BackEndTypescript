04d980af3154d26b92d8dc3e633cb5100d595140
OrquideaFlorida <140519321+OrquideaFlorida@users.noreply.github.com>
2023-10-19 23:41:21 +0000

    resolvendo exercicios

--- Mod03Aula02.ts
+++ Mod03Aula02.ts
@@ -1,6 +1,8 @@
 // Aula 05/10
 // Casses Typescript
 
+import { NumberLiteralType } from "typescript";
+
 //EXEMPLO:
 class Carro {
     // Atributos
@@ -62,16 +64,120 @@ console.log(`Meu nome é ${aPessoa.nome}, tenho ${aPessoa.idade}, anos e meu e-m
 //-- Exercício 3 --
 //CRIE UMA CLASSE RETÂNGULO COM OS ATRIBUTOS ALTURA E LARGURA E MÉTODOS PARA
 //CALCULAR A ÁREA E OPERÍMETRO:
+class Retangulo {
+    altura: number;
+    largura: number;
+
+    constructor(altura: number, largura: number){
+        this.altura = altura;
+        this.largura = largura;
+    }
+    area(){
+        console.log("Sua Área é: " + this.altura * this.largura);
+    }
+    perimetro(){
+        console.log("Seu Perímetro é: " + (2 * this.altura + 2 * this.largura));
+    }
+}
+       
+const oRetangulo = new Retangulo(10, 30);
+console.log(`O Retângulo possui base: ${oRetangulo.largura} e Algura: ${oRetangulo.altura}`);
+
+oRetangulo.area();
+oRetangulo.perimetro();
 
 
 //-- Exercício 4 --
 //CRIE UMA CLASSE CÍRCULO COM O ATRIBUTO RAIO E MÉTODOS PARA CALCULAR A ÁREA E
 //A CIRCUNFERÊNCIA:
+class Circulo {
+    raio: number;
 
+    constructor(raio: number){
+        this.raio = raio;
+    }
+    areaCirc(){
+        console.log("A área do círculo é: " + (3.1416 * this.raio^2));
+    }
+    circunferencia(){
+        console.log("A sua circunferência mede: " + (2 * 3.1416 * this.raio));
+    }
+}
+const oCirculo = new Circulo(10);
+console.log(`O círculo possui raio de: ${oCirculo.raio}`);
+
+oCirculo.areaCirc();
+oCirculo.circunferencia();
 
 //-- Exercício 5 --
 //CRIE UMA CLASSE CONTA BANCÁRIA, COM OS ATRIBUTOS SALDO E numeroConta E MÉTODOS
 // PARA DEPOSITAR, SACAR E VERIFICAR O SALDO
+class ContaBancaria{
+    saldo: number;
+    numeroConta: number;
 
+    constructor(saldo: number, numeroConta: number){
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+    }
+    deposito(){
+
+    }
+    saque(){
+
+    }
+    verifSaldo(){
+
+    }
+}
 
 // -- Exercício 6 --
+//CRIE UMA CLASSE PRODUTO COM OS ATRIBUTOS NOME, PREÇO E QUANTIDADE E MÉTODOS
+//PARA CALCULAR O VALOR TOTAL (PREÇO * QUANTIDADE)
+// class Produto{
+//     nome: string;
+//     preço: number;
+//     quantidade: number;
+
+//     constructor(nome: string, preço: number, quantidade: number){
+//         this.nome = nome;
+//         this.preço = preço;
+//         this.quantidade = quantidade;
+//     }
+//     valorTotal(){
+
+//     }
+// }
+
+// -- Exercício 7 --
+//CRIE UMA CLASSE TRIÂNGULO COM OS ATRIBUTOS LADO1, LADO2, LADO3 E UM MÉTODO
+//PARA SABER SE O TRIÂNCULO É VÁLIDO
+// class Triangulo{
+//     lado1: number;
+//     lado2: number;
+//     lado3: number;
+
+//     constructor(lado1: number, lado2: number, lado3: number){
+//         this.lado1 = lado1;
+//         this.lado2 = lado2;
+//         this.lado3 = lado3;
+//     }
+//     triValido(){
+
+//     }
+// }
+
+// -- Exercício 8 --
+//CRIE UMA CLASSE LIVRO COM ATRIBUTOS TITULO, AUTOR E anoPublicacao E UM MÉTODO PARA
+//EXIBIR INFORMAÇÕES DO LIVRO
+
+
+// -- Exercício 9 --
+//CRIE UMA CLASSE AGENDA, QUE PERMITA ADICIONAR, LISTAR E REMOVER CONTATOS (NOME E NÚMEROS
+// DE TELEFONE)
+
+
+// -- Exercício 10 --
+//CRIE UMA CLASSE carroEsportivo QUE HERDA DA CLASSE CARRO (USADA NO EXEMPLO ANTERIOR) E TENHA
+//MÉTODOS ADICIONAIS PARA ACELERAR RAPIDAMENTE E PRODUZIR UM SOM ESPORTIVO
+
